0d7f58356c07da53cf61be3e0904116cc70118ac
eozturk78 <eozturk78@gmail.com>
2022-08-11 18:21:57 +0300

    translation messages are added

---
+++ src/app/models/consts/enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBh9MPmP-PfNNl8INDyJcOAPC2bbySOzAo",
+    authDomain: "chatinuni-d900d.firebaseapp.com",
+    databaseURL: "https://chatinuni-d900d.firebaseio.com",
+    projectId: "chatinuni-d900d",
+    storageBucket: "chatinuni-d900d.appspot.com",
+    messagingSenderId: "1007774413971",
+    appId: "1:1007774413971:web:1a8485af55cdf2d7bbc966"
+  }
+};
